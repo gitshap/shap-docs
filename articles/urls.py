1f0a195aa70efc7456051e506cf966b6356ea3c9
Carl <48541404+carljcruz@users.noreply.github.com>
2022-03-03 13:19:55 +0800

    addded funcitonality

--- articles/urls.py
+++ articles/urls.py
@@ -2,13 +2,16 @@ from django.urls import path
 from articles import views
 
 urlpatterns = [
-    path('', views.home, name='home'),   
+    path('', views.home, name='home'),
 
 
     # posts
     path('post/create_post/', views.create_post, name='create_post'),
-    
+
     # posts view posts
     path('post/view_post/<int:id>', views.view_post, name='view_post'),
     path('post/update_post/<int:id>', views.update_post, name='update_post'),
+    path('post/delete_post/<int:id>', views.delete_post, name='delete_post'),
+    path('post/all_post/<int:id>', views.all_post, name='all_post'),
+
 ]
--- articles/views.py
+++ articles/views.py
@@ -4,18 +4,46 @@ from .models import Article, Post
 from .forms import PostForm
 
 
-
 def home(request):
     template_name = 'articles/articles.html'
     article = Article.objects.all()
 
-
     get_network = Article.objects.get(title='Networks')
     networks = Post.objects.filter(article=get_network)
+
+    get_licenses = Article.objects.get(title='Licenses')
+    licenses = Post.objects.filter(article=get_licenses)
+
+    get_server_2019 = Article.objects.get(title='Windows Server 2019')
+    server_2019 = Post.objects.filter(article=get_server_2019)
+
+    get_processes = Article.objects.get(title='Internal IT Processes')
+    processes = Post.objects.filter(article=get_processes)
+
+    get_google = Article.objects.get(title='Google Workspace')
+    google = Post.objects.filter(article=get_google)
+
+    get_workstations = Article.objects.get(title='Workstations')
+    workstation = Post.objects.filter(article=get_workstations)
+
+    get_nas = Article.objects.get(title='NAS')
+    nas = Post.objects.filter(article=get_nas)
+
+    get_tools = Article.objects.get(title='Internal Tools')
+    tools = Post.objects.filter(article=get_tools)
+
     context = {
         'article': article,
 
-        'networks': networks
+        'networks': networks,
+        'licenses': licenses,
+        'server_2019': server_2019,
+        'processes': processes,
+        'google': google,
+        'workstation': workstation,
+        'nas': nas,
+        'tools': tools
+
     }
     return render(request, template_name, context=context)
 
@@ -46,7 +74,6 @@ def view_post(request, id):
     return render(request, template_name, context=context)
 
 
-
 def update_post(request, id):
     template_name = 'posts/update_post.html'
     get_post = Post.objects.get(id=id)
@@ -60,4 +87,56 @@ def update_post(request, id):
     context = {
         'form': form
     }
+    return render(request, template_name, context=context)
+
+
+def delete_post(request, id):
+    template_name = 'posts/delete_post.html'
+    get_post = Post.objects.get(id=id)
+    get_post.delete()
+    return render(request, template_name, context=None)
+
+
+def all_post(request, id):
+
+    template_name = 'posts/all_post.html'
+    article = Article.objects.get(id=id)
+    get_network = Article.objects.get(title='Networks')
+    networks = Post.objects.filter(article=get_network)
+
+    get_licenses = Article.objects.get(title='Licenses')
+    licenses = Post.objects.filter(article=get_licenses)
+
+    get_server_2019 = Article.objects.get(title='Windows Server 2019')
+    server_2019 = Post.objects.filter(article=get_server_2019)
+
+    get_processes = Article.objects.get(title='Internal IT Processes')
+    processes = Post.objects.filter(article=get_processes)
+
+    get_google = Article.objects.get(title='Google Workspace')
+    google = Post.objects.filter(article=get_google)
+
+    get_workstations = Article.objects.get(title='Workstations')
+    workstation = Post.objects.filter(article=get_workstations)
+
+    get_nas = Article.objects.get(title='NAS')
+    nas = Post.objects.filter(article=get_nas)
+
+    get_tools = Article.objects.get(title='Internal Tools')
+    tools = Post.objects.filter(article=get_tools)
+
+    context = {
+        'article': article,
+
+        'networks': networks,
+        'licenses': licenses,
+        'server_2019': server_2019,
+        'processes': processes,
+        'google': google,
+        'workstation': workstation,
+        'nas': nas,
+        'tools': tools
+
+    }
+
     return render(request, template_name, context=context)
